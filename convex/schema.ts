571e47dd0e1e3114d328bc6cb23ad1a4ae59f4bc
Abel de Bruijn <abeldebruijn@hotmail.com>
2026-04-02 19:51:17 +0200

    Add exams, questions and practiceSessions tables

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,12 +1,38 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-// The schema is entirely optional.
-// You can delete this file (schema.ts) and the
-// app will continue to work.
-// The schema provides more precise TypeScript types.
 export default defineSchema({
   numbers: defineTable({
     value: v.number(),
+  }),
+
+  exams: defineTable({
+    name: v.string(),
+    year: v.number(),
+    month: v.number(),
+  }),
+
+  questions: defineTable({
+    number: v.number(),
+    question: v.string(),
+    options: v.array(v.string()),
+    answer: v.number(),
+    literatureReference: v.optional(v.string()),
+    feedback: v.optional(v.string()),
+    feedbackPrompt: v.optional(v.string()),
+    exam: v.id("exams"),
+    help: v.optional(v.record(v.string())), // {A: string, B: string}
+  }),
+
+  practiceSessions: defineTable({
+    exam: v.id("exams"),
+    userId: v.string(),
+    score: v.number(),
+    dataCreated: v.string(),
+    timeStarted: v.string(),
+    timeEnded: v.string(),
+    questions: v.array(v.id("questions")),
+    type: v.union(v.literal("MultipleChoice"), v.literal("OpenEnded")),
+    allowRetries: v.boolean(),
   }),
 });
